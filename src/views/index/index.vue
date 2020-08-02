99e782d4a16446f6372c4337bc6a05d9c7e2781e
hpulegion <hpulegion@163.com>
2020-08-02 16:57:33 +0800

    项目初始化及配置

---
+++ src/views/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id=''>
+    首页
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang='scss'>
+
+</style>
